3c90fbefe3c4ebe830eb3225560248c432a71237
hba <87194281+hubzhooba@users.noreply.github.com>
2025-03-05 02:01:05 -0800

    update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -34,7 +34,9 @@ export function About() {
             My curiosity for computers began at age 13, fueled by countless hours playing Counter-Strike and Warcraft, which naturally led me to pursue a career in tech. I&apos;ve been working as a software engineer, specializing in blockchain development and design, for over 3 years!
             </p>
             <p className="text-xl text-muted-foreground">
-            Alongside my Web3 journey, I run a marketing agency built on the principle of 100% ownership—because you deserve to keep every bit of your creative work. We bridge creators and local brands to forge powerful connections and create compelling digital strategies. When I’m not at my desk, you can find me outdoors soaking in nature’s energy.
+            Alongside my Web3 journey, I run a marketing agency built on the principle of 100% ownership—because you deserve to keep every bit of your creative work. We bridge creators and local brands to forge powerful connections and create compelling digital strategies. When I&apos;m not at my desk, you can find me outdoors soaking in nature&apos;s energy.
+
+            </p>
             <p className="text-xl text-muted-foreground">
             Health, Family & Friends. And above all -  Purpose…
             </p>
@@ -55,5 +57,5 @@ export function About() {
         </motion.div>
       </div>
     </motion.section>
-  );
+  )
 }
